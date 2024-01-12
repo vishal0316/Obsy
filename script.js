d517c4931e55e84883a7f4e2e2936c5d76e4c916
vishal0316 <vr987195@gmail.com>
2024-01-12 17:26:57 +0530

    loaderdone

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
-gsap.from(".line h1", {
+var tl = gsap.timeline();
+tl.from(".line h1", {
   y: 150,
   stagger: 0.25,
   duration: 0.5,
   delay: 0.5,
 });
+
+tl.from(".line1-part1 , .line h2", {
+  opacity: 0,
+  onStart: function () {
+    var h5timer = document.querySelector("#line1-part1 h5");
+    var grow = 0;
+    setInterval(function () {
+      if (grow < 100) {
+        h5timer.innerHTML = grow++;
+      } else {
+        h5timer.innerHTML = grow;
+      }
+    }, 27);
+  },
+});
+
+tl.to("#loader"),
+  {
+    opacity: 0,
+    duration: 0.4,
+    delay: 0.4,
+  };

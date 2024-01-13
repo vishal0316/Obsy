d85f356ac03571cec4ef913ba834682541083ffa
vishal0316 <vr987195@gmail.com>
2024-01-13 16:33:49 +0530

    page1

--- script.js
+++ script.js
@@ -39,5 +39,23 @@ function loadingAnimation() {
   tl.to("#loader", {
     display: "none",
   });
+  tl.from("#nav", {
+    opacity: 0,
+  });
+  tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1", {
+    y: 120,
+    stagger: 0.2,
+  });
+}
+function cursorAnimation() {
+  document.addEventListener("mousemove", function (dets) {
+    gsap.to("#crsr", {
+      left: dets.x,
+      top: dets.y,
+    });
+  });
+
+  Shery.makeMagnet("#nav-part2 h4");
 }
 loadingAnimation();
+cursorAnimation();

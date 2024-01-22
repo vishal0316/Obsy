cdd11292a7dc47bcd15c587c9ea9da897426dcd2
vishal0316 <vr987195@gmail.com>
2024-01-22 14:01:30 +0530

    responsive

--- script.js
+++ script.js
@@ -148,37 +148,45 @@ function cursorAnimation() {
     }
   });
 }
-function sheryAnimation() {
-  Shery.imageEffect(".image-div", {
-    style: 5,
-    gooey: true,
-    // debug:true,
-    config: {
-      a: { value: 2, range: [0, 30] },
-      b: { value: 0.75, range: [-1, 1] },
-      zindex: { value: -9996999, range: [-9999999, 9999999] },
-      aspect: { value: 0.7241195453907675 },
-      gooey: { value: true },
-      infiniteGooey: { value: false },
-      growSize: { value: 4, range: [1, 15] },
-      durationOut: { value: 1, range: [0.1, 5] },
-      durationIn: { value: 1.5, range: [0.1, 5] },
-      displaceAmount: { value: 0.5 },
-      masker: { value: true },
-      maskVal: { value: 1.23, range: [1, 5] },
-      scrollType: { value: 0 },
-      geoVertex: { range: [1, 64], value: 1 },
-      noEffectGooey: { value: true },
-      onMouse: { value: 0 },
-      noise_speed: { value: 0.5, range: [0, 10] },
-      metaball: { value: 0.33, range: [0, 2] },
-      discard_threshold: { value: 0.5, range: [0, 1] },
-      antialias_threshold: { value: 0.01, range: [0, 0.1] },
-      noise_height: { value: 0.5, range: [0, 2] },
-      noise_scale: { value: 10, range: [0, 100] },
-    },
-  });
+
+if (
+  !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+    navigator.userAgent
+  )
+) {
+  function sheryAnimation() {
+    Shery.imageEffect(".image-div", {
+      style: 5,
+      gooey: true,
+      // debug:true,
+      config: {
+        a: { value: 2, range: [0, 30] },
+        b: { value: 0.75, range: [-1, 1] },
+        zindex: { value: -9996999, range: [-9999999, 9999999] },
+        aspect: { value: 0.7241195453907675 },
+        gooey: { value: true },
+        infiniteGooey: { value: false },
+        growSize: { value: 4, range: [1, 15] },
+        durationOut: { value: 1, range: [0.1, 5] },
+        durationIn: { value: 1.5, range: [0.1, 5] },
+        displaceAmount: { value: 0.5 },
+        masker: { value: true },
+        maskVal: { value: 1.23, range: [1, 5] },
+        scrollType: { value: 0 },
+        geoVertex: { range: [1, 64], value: 1 },
+        noEffectGooey: { value: true },
+        onMouse: { value: 0 },
+        noise_speed: { value: 0.5, range: [0, 10] },
+        metaball: { value: 0.33, range: [0, 2] },
+        discard_threshold: { value: 0.5, range: [0, 1] },
+        antialias_threshold: { value: 0.01, range: [0, 0.1] },
+        noise_height: { value: 0.5, range: [0, 2] },
+        noise_scale: { value: 10, range: [0, 100] },
+      },
+    });
+  }
 }
+
 function flagAnimation() {
   document.addEventListener("mousemove", function (dets) {
     gsap.to("#flag", {
